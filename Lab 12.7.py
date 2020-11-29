c7767b3bf041f727766b9e4bec1de6da15ed7522
Andrew <andrew.li729@gmail.com>
2020-11-29 03:35:54 -0600

    defined get age function and implemented ValueError exception

--- Lab 12.7.py
+++ Lab 12.7.py
@@ -3,13 +3,17 @@
 
 def fat_burning_heart_rate(age):
     rate = 0.7 * (220 - age)
-    return rate
+    print("Fat burning hear rate for a {} year-old: {} bpm".format(age, rate))
 
 
 def get_age():
-    pass
+    age_input = int(input("Enter age:"))
+    return age_input
 
 
 if __name__ == '__main__':
-    test = fat_burning_heart_rate(int(input()))
-    print(test)
+    age = get_age()
+    if age < 18 or age > 75:
+        raise ValueError("Invalid Age.\nCould not calculate heart rate info.")
+    fat_burning_heart_rate(age)
+
