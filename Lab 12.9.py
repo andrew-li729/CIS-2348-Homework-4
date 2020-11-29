1d3f41a1a3e44845e39b75bbd610af834a495515
Andrew <andrew.li729@gmail.com>
2020-11-29 15:38:07 -0600

    implemented try and except

--- Lab 12.9.py
+++ Lab 12.9.py
@@ -5,10 +5,12 @@
 parts = input().split()
 name = parts[0]
 while name != '-1':
-    # FIXME: The following line will throw ValueError exception.
-    #        Insert try/except blocks to catch the exception.
-    age = int(parts[1]) + 1
-    print('{} {}'.format(name, age))
+    try:
+        age = int(parts[1]) + 1
+        print('{} {}'.format(name, age))
+    except ValueError:  # if converting part[1] to int gives an error, do this
+        age = 0         # sets age to zero if second part of input is not an integer
+        print('{} {}'.format(name, age))
 
     # Get next line
     parts = input().split()
